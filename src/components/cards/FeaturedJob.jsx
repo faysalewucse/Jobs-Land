14340d4dbbfad99bb4ed25bd786530f80c2cbf19
faysalewucse <faysal.ewucse@gmail.com>
2023-04-12 00:23:44 +0600

    filter dropdown working perfectly

--- src/components/cards/FeaturedJob.jsx
+++ src/components/cards/FeaturedJob.jsx
@@ -20,36 +20,52 @@ export default function FeaturedJob({ job, horizontal }) {
   } = job;
   const navigate = useNavigate();
   return (
-    <div className="border border-indigo-100 p-5 rounded-md flex flex-col gap-2">
-      <img
-        className="w-20 h-20 object-contain"
-        src={companyLogo}
-        alt="company-logo"
-      />
-      <h1 className="font-bold text-xl">{jobTitle}</h1>
-      <h1>{companyName}</h1>
-      <div className="flex gap-3">
-        <h1 className="w-fit border border-indigo-500 rounded p-2 text-center text-indigo-500">
-          {jobType}
-        </h1>
-        <h1 className="w-fit border border-indigo-500 rounded p-2 text-center text-indigo-500">
-          {time}
-        </h1>
-      </div>
-      <div className="flex gap-5">
-        <h1 className="flex gap-2 items-center">
-          <FontAwesomeIcon icon={faLocationDot} />
-          {address}
-        </h1>
-        <h1 className="flex gap-2 items-center">
-          <FontAwesomeIcon icon={faCircleDollarToSlot} />
-          {salary}
-        </h1>
+    <div
+      className={`border border-indigo-100 p-5 rounded-md flex ${
+        !horizontal ? "flex-col" : "justify-between"
+      } gap-2`}
+    >
+      <div
+        className={`flex ${!horizontal ? "flex-col" : "justify-between"} gap-4`}
+      >
+        <img
+          className={`${
+            horizontal
+              ? "my-auto bg-gray-100 p-5 w-40 h-40 rounded"
+              : "w-20 h-20"
+          } object-contain`}
+          src={companyLogo}
+          alt="company-logo"
+        />
+        <div
+          className={`flex flex-col gap-2 ${horizontal && "justify-center"}`}
+        >
+          <h1 className="font-bold text-xl">{jobTitle}</h1>
+          <h1>{companyName}</h1>
+          <div className="flex gap-3">
+            <h1 className="w-fit border border-indigo-500 rounded p-2 text-center text-indigo-500">
+              {jobType}
+            </h1>
+            <h1 className="w-fit border border-indigo-500 rounded p-2 text-center text-indigo-500">
+              {time}
+            </h1>
+          </div>
+          <div className="flex gap-5">
+            <h1 className="flex gap-2 items-center">
+              <FontAwesomeIcon icon={faLocationDot} />
+              {address}
+            </h1>
+            <h1 className="flex gap-2 items-center">
+              <FontAwesomeIcon icon={faCircleDollarToSlot} />
+              {salary}
+            </h1>
+          </div>
+        </div>
       </div>
       <PrimaryButton
         onClickHandler={() => navigate(`/jobdetails/${id}`)}
         text={"View Details"}
-        style={"w-1/4"}
+        style={`${!horizontal ? "w-1/4" : "h-fit my-auto"}`}
       />
     </div>
   );
--- src/pages/AppliedJobs.jsx
+++ src/pages/AppliedJobs.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import FeaturedJob from "../components/cards/FeaturedJob";
 
 export default function AppliedJobs() {
+  const [filter, setFilter] = useState("all");
   const [appliedJobs, setAppliedJobs] = useState(
     JSON.parse(localStorage.getItem("appliedJobs")) || []
   );
@@ -13,20 +14,30 @@ export default function AppliedJobs() {
       <BottomNavbar text={"Applied Jobs"} />
       <img className="top-0 right-0 absolute" src={vector1} alt="vector1" />
       <div className="max-w-7xl mx-auto p-20">
-        <div>
-          <select
-            name="jobtype"
-            id="jobtype"
-            className="block w-1/8 border border-indigo-200 rounded py-2 px-5 float-right outline-none"
-          >
-            <option value="remote">Remote</option>
-            <option value="onsite">On Site</option>
-          </select>
-        </div>
-        <div className="">
-          {appliedJobs?.map((job) => (
-            <FeaturedJob key={job.id} job={job} horizontal={true} />
-          ))}
+        <select
+          name="jobtype"
+          id="jobtype"
+          className="border border-indigo-200 rounded py-2 px-5 float-right outline-none"
+          defaultValue={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="Remote">Remote</option>
+          <option value="On Site">On Site</option>
+        </select>
+        <br />
+        <br />
+        <br />
+        <div className="flex flex-col gap-5">
+          {filter !== "all"
+            ? appliedJobs
+                .filter((job) => job.jobType === filter)
+                .map((job) => (
+                  <FeaturedJob key={job.id} job={job} horizontal={true} />
+                ))
+            : appliedJobs.map((job) => (
+                <FeaturedJob key={job.id} job={job} horizontal={true} />
+              ))}
         </div>
       </div>
     </div>
